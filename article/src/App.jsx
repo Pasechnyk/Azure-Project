1dc7ed433ec084c07bdc0e484fd24c60cc331a46
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-11-25 14:25:22 +0200

    Add files via upload

---
+++ article/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Article from './components/Article';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Jorunna parva</h1>
+      <Article/>
+    </div>
+  );
+}
---
+++ article/src/components/Article.jsx
@@ -0,0 +1,42 @@
+import "./Article.css";
+
+export default function Article() {
+    return (
+    <div>
+        <img src="https://marinemadnessdotblog.files.wordpress.com/2019/04/sea-bunny-jorunna-parva.jpg"></img>
+
+        <p>
+        Jorunna parva, commonly known as the sea bunny,
+        is a species of dorid nudibranch, a shell-less
+        marine gastropod mollusc in the family Discodorididae.
+        The species was first described by Kikutaro Baba.
+        Its resemblance to a rabbit facilitated a surge in
+        popularity on Twitter throughout Japan in 2015.</p>
+
+        <h4>Description</h4>
+        <p>The species is about 1 cm long on average.
+            Its black-and-white rhinophores somewhat
+            resemble a rabbit's ears. Its external gills
+            are located near its rear. Its body is covered
+            in papillae, fleshy protuberances used for sensory
+            functions, giving it the appearance of a furry animal.
+            There are multiple colorations of Jorunna parva,
+            including yellow, white, and green, though the latter
+            is rarely photographed. All of these variants have
+            black papillae interspersed among papillae of their
+            main color. There is controversy over whether or not
+            the different colorations are divergent species.</p>
+
+        <h4>Distribution</h4>
+        <p>This species was described from Kii Province, Japan.
+            Jorunna parva has subsequently been reported from
+            the Philippines, Tanzania, Papua New Guinea, Seychelles
+            and Réunion but there are some doubts as to whether it
+            is really a species complex. The Jorunna parva are
+            concentrated in areas where there is an abundance
+            of food and where resources are easily acquired.
+            They often cling to submerged vegetation and spend
+            majority of the time at the bottom of tropical waters.</p>
+    </div>
+    );
+}
